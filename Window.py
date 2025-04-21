76f0fbc340eaf2ae47c7c845b76f5f4fbc11c281
Pauly-VV <pvermeulen26@gmail.com>
2025-04-21 19:05:53 +0200

    Created the point and line classes, and the methods to draw lines, using points. Also, fixed so that the dimensions are defined in the canvas, instead of the window, for consistent coordinates.

--- Window.py
+++ Window.py
@@ -2,16 +2,13 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
         self.root = Tk()
-        self.root.title("Window")
-        self.root.geometry(f"{width}x{height}")
+        self.root.title("Maze Solver")
+        #self.root.geometry(f"{width}x{height}")
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.canvas = Canvas(self.root)
-        self.canvas.pack()
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=True)
 
         self.windowRunning = False
 
@@ -26,6 +23,10 @@ class Window():
         
     def close(self):
         self.windowRunning = False
+
+    def draw_line(self, line, fill_colour = "black"):
+        line.draw(self.canvas, fill_colour)
+
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from Window import *
+from Point import *
+from Line import *
 
 def main():
     win = Window(800, 600)
+
+    line1 = Line(Point(0, 0), Point(400, 400))
+    line2 = Line(Point(800, 0), Point(0, 400))
+
+    win.draw_line(line1)
+    win.draw_line(line2, "blue")
+
     win.waitforclose()
 
 main()
